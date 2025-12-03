b42768d999d8a73870619ff247647b8324bf67f8
DanielVilaca <a16939@alunos.ipca.pt>
2025-12-03 01:55:58 +0000

    Training Model

--- Email_Clustering.py
+++ Email_Clustering.py
@@ -5,9 +5,16 @@ from sklearn.feature_extraction import _stop_words
 from sklearn.cluster import KMeans
 
 
-
 #importing email dataset
 df = pd.read_csv('./emails.csv')
 print(df.shape)
 df = df.dropna()
 print(df.shape)
+
+#extracting features to train the model
+stop_words = _stop_words.ENGLISH_STOP_WORDS
+cvect = CountVectorizer(stop_words=list(stop_words))
+X_train_matrix = cvect.fit_transform(df['message'].astype(str))
+kmeans = KMeans(n_clusters=10, random_state=42, n_init=10)
+kmeans.fit(X_train_matrix)
+print("Clustering Completed!")
