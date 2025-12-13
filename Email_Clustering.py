fbe9ea9d79a6ea66b66e8a685068a601ab1ac546
DanielVilaca <a16939@alunos.ipca.pt>
2025-12-13 15:13:56 +0000

    Visualization of Clusters + Aditional stop words

--- Email_Clustering.py
+++ Email_Clustering.py
@@ -4,6 +4,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction import _stop_words
 from sklearn.cluster import KMeans
 
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
 
 #importing email dataset
 df = pd.read_csv('./emails.csv')
@@ -15,7 +18,7 @@ print(df.shape)
 stop_words = _stop_words.ENGLISH_STOP_WORDS
 aditional_stopwords = ["0f", "attached", "cc", "com", "corp", "doc", "ect", "ees", "email",
                        "enron","enronxgate", "forwarded","hou","mail","message","na","new","original","pm",
-                       "recipient", "said","sent","subject","year","2000","2001"]
+                       "recipient", "said","sent","subject","year","2000","2001", "bcc", "ou", "cn"]
 
 smallnum_stopwords = [str(i).zfill(2) for i in range(100)]
 stop_words = stop_words.union(smallnum_stopwords)
@@ -35,6 +38,36 @@ for i, center in enumerate(centre):
     top_words_indices = np.argsort(center)[::-1][:20]
     top_words = [words[ind] for ind in top_words_indices]
     print(f"Top 20 Words: Cluster {i}: {top_words}")
+
+#visualization of clusters 0 and 4
+def create_wordcloud(cluster_index, ax):
+    center = centre[cluster_index]
+    top_words_indices = np.argsort(center)[::-1][:20]
+    word_weights = {words[ind]: center[ind] for ind in top_words_indices}
+
+    wc = WordCloud(background_color='white', width=400, height=200)
+    wc.generate_from_frequencies(word_weights)
+
+    ax.imshow(wc, interpolation='bilinear')
+    ax.axis('off')
+    ax.set_title(f"Top 20 Words: Cluster {cluster_index}", fontsize=14)
+
+#figure with 2 columns
+fig, axes = plt.subplots(1, 2, figsize=(15, 6))
+fig.suptitle('Visualising Email Clusters', fontsize=20)
+
+#clouds for Cluster 0 and Cluster 4
+create_wordcloud(0, axes[0])
+create_wordcloud(4, axes[1])
+
+plt.tight_layout()
+
+#saving image
+filename = 'email_clusters_visualisation.png'
+plt.savefig(filename, dpi=300)
+print(f"Image successfully saved as: {filename}")
+
+plt.close()
 
 #list cluster counts
 clusters = kmeans.labels_
