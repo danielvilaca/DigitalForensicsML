e303a62914dce7fbcfac6995567c9396c7f6d818
DanielVilaca <a16939@alunos.ipca.pt>
2025-12-18 19:16:36 +0000

    New features -> DecisionTree 76%

--- log_classification.py
+++ log_classification.py
@@ -16,6 +16,25 @@ from sklearn.svm import LinearSVC
 from time import perf_counter
 from sklearn.metrics import accuracy_score, confusion_matrix
 
+from urllib.parse import urlparse, parse_qs
+import re
+
+
+def extract_features(url):
+    parsed_url = urlparse(url)
+    features = {
+        'url_length': len(url),
+        'alphabets_count': sum(c.isalpha() for c in url),
+        'special_characters_count': sum(c in ['-', '_', '@', '/', '?', '=', '.', '%', '&', '!', ';', '+', ',', '*', '~', '|'] for c in url),
+        'digits_count': sum(c.isdigit() for c in url),
+        'has_https': 1 if "https" in url else 0,
+        'has_ip': 1 if re.search(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', url) else 0,
+        'subdomain_count': url.count('.'),
+        'parameters_count': len(parse_qs(parsed_url.query)),
+        'suspicious_tlds': 1 if any(tld in url for tld in ['.zip', '.rar', '.exe', '.dmg', '.sh', '.bin', '.xyz', '.top', '.pw', '.icu', '.tk', '.ga', '.cf', '.ml', '.bit']) else 0
+    }
+    return features
+
 
 
 #data load
@@ -31,6 +50,11 @@ vect = TfidfVectorizer()
 X = vect.fit_transform(data['text'])
 y = data['label']
 
+
+data = pd.concat([data, data['text'].apply(lambda x: pd.Series(extract_features(x)))], axis=1)
+X = data.drop(columns=['text', 'label'])
+#print(X.head())
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=True, random_state=42)
 
 models = {
@@ -41,15 +65,22 @@ models = {
     'SVM (Linear)': LinearSVC(dual=False)
 }
 
-# for name, model in models.items():
-#     print(f"Training mode {name}, standby...")
-#     start_time = perf_counter()
-#     model.fit(X_train, y_train)
-#     end_time = perf_counter()
-#     time_taken = end_time - start_time
-#     print(f"Training time: {time_taken:.2f} secs")
-#     y_pred = model.predict(X_test)
-#     accuracy = accuracy_score(y_test, y_pred)
-#     print(f"Accuracy Percentage: {accuracy*100:.2f}%")
-#     print(f"-----------------------------------------")
+for name, model in models.items():
+    print(f"Training mode {name}, standby...")
+    start_time = perf_counter()
+    model.fit(X_train, y_train)
+    end_time = perf_counter()
+    time_taken = end_time - start_time
+    print(f"Training time: {time_taken:.2f} secs")
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Accuracy Percentage: {accuracy*100:.2f}%")
+    print(f"-----------------------------------------")
+
+# model = LinearSVC(dual=False, random_state=42, max_iter=50000)
+# final_model = model.fit(X_train, y_train)
+# y_pred = final_model.predict(X_test)
+
+# test_url = pd.Series('www.G00gle.com/someevil.php')
+# print(final_model.predict(vect.transform(test_url))[0])
 
